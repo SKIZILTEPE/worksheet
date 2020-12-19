f16c61db017384b49c779eef7722bf79ac029848
savas <savaskiziltepe@hotmail.com>
2020-12-19 16:51:38 +0300

    init

---
+++ scriptkodlarım/script.js
@@ -0,0 +1,14 @@
+document.write("ilk javaScript kodlarım");
+alert("merhaba dünya")
+
+function process(){
+document.getElementById('result').innerHTML="nasılmış?"
+}
+
+function openLight(){
+document.getElementById('picture').src="css/ampul açık.gif";
+}
+
+function closeLight(){
+    document.getElementById('picture').src="css/ampul kapalı.gif";
+}
